04bf85dc08a7800109b184855d4c73c41c1d89cf
Emanuel Pereira <emanuelcdpr@gmail.com.br>
2025-08-25 19:36:05 -0300

    feat: add routes and tests

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,8 +1,52 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
+database = []
 
 app = FastAPI()
 
 
-@app.get('/')
-def read_root():
-    return {'message': 'Olá Mundo'}
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.get('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def read_user(user_id: int):
+    for data in database:
+        if (data.id == user_id):
+            return data
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User Not Found'
+        )
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User Not Found'
+        )
+
+    del database[user_id - 1]
+    return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,82 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from src.fast_zero.app import app
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'John Doe',
+            'password': 'passwd',
+            'email': 'john.doe@mail.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'email': 'john.doe@mail.com',
+        'id': 1,
+        'username': 'John Doe',
+    }
 
 
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-    response = client.get('/')
+def test_read_users(client):
+    response = client.get('/users')
+
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo'}
+    assert response.json() == {
+        'users': [
+            {
+                'email': 'john.doe@mail.com',
+                'id': 1,
+                'username': 'John Doe',
+            }
+        ]
+    }
+
+
+def test_update_user_error(client):
+    response = client.put(
+        'users/10',
+        json={
+            'username': 'Jane Doe',
+            'email': 'jane_doe@mail.com',
+            'id': 1,
+            'password': 'passwd',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    body = response.json()
+    assert body['detail'] == 'User Not Found'
+
+
+def test_update_user(client):
+    response = client.put(
+        'users/1',
+        json={
+            'username': 'Jane Doe',
+            'email': 'jane_doe@mail.com',
+            'id': 1,
+            'password': 'passwd',
+        },
+    )
+
+    assert response.json() == {
+        'username': 'Jane Doe',
+        'email': 'jane_doe@mail.com',
+        'id': 1,
+    }
+
+
+def test_delete_user_error(client):
+    response = client.delete('users/10')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User Not Found'}
+
+
+def test_delete_user(client):
+    response = client.delete('users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
